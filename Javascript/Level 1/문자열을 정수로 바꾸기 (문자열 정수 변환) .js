4339ab3a97dee52168f38b4f2ae1760bf31b4d1b
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-07-26 21:54:35 +0900

    문자열 정수 변환

---
+++ Javascript/Level 1/문자열을 정수로 바꾸기 (문자열 정수 변환) .js
@@ -0,0 +1,10 @@
+function solution(s) {
+    var answer = 0;
+    
+    answer= Number(s);		//parseInt(s)나 s/1, s*1 등도 가능하다.
+    
+    return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12925?language=javascript
+// 문자열 정수 변환
