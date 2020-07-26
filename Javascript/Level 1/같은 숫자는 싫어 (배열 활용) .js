5c8ff1c1b49a9c0cc57809402917bfe7915dc851
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-07-27 00:09:01 +0900

    배열 활용해서 다음에 오는 값 비교하는 문제

---
+++ Javascript/Level 1/같은 숫자는 싫어 (배열 활용) .js
@@ -0,0 +1,14 @@
+function solution(arr) {
+    var answer = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i + 1]) {
+            answer.push(arr[i]);
+        }
+    }
+
+    return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12906?language=javascript
+// 배열 활용해서 다음에 오는 값 비교하는 문제
