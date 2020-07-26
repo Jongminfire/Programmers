4b36fa5676aa8012536d108f0ef5bf199f018a5b
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-07-26 21:47:41 +0900

    문자열 길이에 따라 문자열의 중간 글자 반환 하는 문제

---
+++ Javascript/Level 1/가운데 글자 가져오기 (문자열처리) .js
@@ -0,0 +1,15 @@
+function solution(s) {
+    var answer = '';
+
+    if (s.length % 2 === 1) {
+        answer = s[s.length / 2 - 0.5];
+    }
+    else {
+        answer = s[s.length / 2 - 1] + s[s.length / 2];
+    }
+
+    return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12903?language=javascript
+// 문자열 길이에 따라 문자열의 중간 글자 반환 하는 문제
