8c7e31eb50be7f85b0438090d367e5c2d48efdcb
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-07-31 00:17:34 +0900

    반복문 활용으로 약수 구하는 문제

---
+++ Javascript/Level 1/약수의 합 (반복문).js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+
+    for (var i = 1; i <= n; i++) {
+        if (n % i === 0)
+            answer += i;
+    }
+
+    return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12928?language=javascript
+// 반복문 활용해서 약수를 구하는 문제
