30503d3d7c2a6bc5d3b2db35fdc5d41894a37a59
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-08-01 00:33:21 +0900

    문자열 p와 y의 개수를 세는 문제

---
+++ Javascript/Level 1/문자열 내 p와 y의 개수 (문자열).js
@@ -0,0 +1,23 @@
+function solution(s) {
+    var answer = true;
+    var count_p = 0;
+    var count_y = 0;
+
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] === 'p' || s[i] === 'P')
+            count_p++;
+
+        else if (s[i] === 'y' || s[i] === 'Y')
+            count_y++;
+    }
+
+    if (count_p === count_y)
+        answer = true;
+    else
+        answer = false;
+
+    return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12916?language=javascript
+// 반복문으로 문자열 p와 y의 개수를 세는 문제
