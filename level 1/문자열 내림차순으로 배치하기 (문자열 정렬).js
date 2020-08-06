0457e147c69bd9d92fd4ef442141953d2f06d22a
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-08-06 21:52:29 +0900

    정렬함수로 문자열 내림차순 정렬

---
+++ level 1/문자열 내림차순으로 배치하기 (문자열 정렬).js
@@ -0,0 +1,25 @@
+function solution(s) {
+    var answer = '';
+    var arr = [];
+
+    for (var i = 0; i < s.length; i++) {
+        arr.push(s[i]);
+    }
+
+    arr.sort(sortReverse);
+
+    for (var i = 0; i < arr.length; i++) {
+        answer += arr[i];
+    }
+
+    return answer;
+}
+
+function sortReverse(a, b) {
+    if (a > b) return -1;
+    else if (b > a) return 1;
+    else return 0;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12917?language=javascript
+// 정렬 함수 정의해서 문자열 내림차순으로 정렬하는 문제
