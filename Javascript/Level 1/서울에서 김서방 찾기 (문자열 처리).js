12506ec40a31408b08b94dbba6f19c41a3970548
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-08-08 00:20:20 +0900

    문자열 'Kim'의 위치 찾기

---
+++ Javascript/Level 1/서울에서 김서방 찾기 (문자열 처리).js
@@ -0,0 +1,16 @@
+function solution(seoul) {
+	var answer = '김서방은 ';
+
+	for (var i = 0;i < seoul.length;i++)
+	{
+		if (seoul[i] === 'Kim')
+		{
+			answer += (i + '에 있다');
+		}
+	}
+
+	return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12919?language=javascript
+// 문자열 'Kim'의 위치 찾기
