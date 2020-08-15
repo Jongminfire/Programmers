cb98c0609859d5f0ee2ad773650115cded9e742b
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-08-16 00:17:14 +0900

    정렬 후 다른 값 찾기

---
+++ Javascript/Level 1/예산(정렬).js
@@ -0,0 +1,18 @@
+function solution(participant, completion) {
+    var answer = '';
+
+    participant.sort();
+    completion.sort();
+
+    for (var i = 0; i < participant.length; i++) {
+        if (participant[i] !== completion[i]) {
+            answer = participant[i];
+            return answer;
+        }
+    }
+
+    return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/42576?language=javascript
+// 정렬 후 다른 값 찾기
