f28120f67f4590ba41107a2fc910f0f4ba664a6c
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-08-17 01:41:36 +0900

    배열 요소 추가

---
+++ Javascript/Level 1/+ x만큼 간격이 있는 n개의 숫자 (배열) .js
@@ -0,0 +1,12 @@
+function solution(x, n) {
+    var answer = [];
+
+    for (var i = 1; i <= n; i++) {
+        answer.push(x * i);
+    }
+
+    return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12954
+// 배열 요소 추가
