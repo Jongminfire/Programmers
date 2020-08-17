12cac9dd73e29a9be91a218c096dc7ce909f2902
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-08-18 00:02:33 +0900

    int와 string 변환

---
+++ Javascript/Level 1/하샤드 수 (Int String 형변환).js
@@ -0,0 +1,18 @@
+function solution(x) {
+    let sum = 0;
+    let string = string(x);
+
+    for (let i = 0; i < string.length; i++) {
+        sum += number(string[i]);
+    }
+
+    if (x % sum) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12947
+// int와 string 형 변환
