79f7f5a1453e0030149623b9703ccff16dcc9c5a
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-04-21 21:37:53 +0900

    주어진 수의 합과 차 나누어 구하기

---
+++ Python/Level 1/음양 더하기 (구현).py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = 0
+    for n, s in zip(absolutes, signs):
+        if s:
+            answer += n
+        else:
+            answer += -n
+
+    return answer
